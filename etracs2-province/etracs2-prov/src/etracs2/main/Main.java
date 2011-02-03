9d7f9a2862bffb3b972c1fa427e2823d7f963ddd
unknown <prmf@.(none)>
2011-02-03 15:06:21 -0800

    modify etracs2-province project

--- etracs2-province/etracs2-prov/src/etracs2/main/Main.java
+++ etracs2-province/etracs2-prov/src/etracs2/main/Main.java
@@ -1,14 +1,24 @@
 package etracs2.main;
 
 import com.rameses.osiris2.client.OsirisTestPlatform;
+import java.io.ObjectInputStream;
 import java.util.HashMap;
 import java.util.Map;
+import javax.swing.LookAndFeel;
+import javax.swing.UIManager;
 
 public class Main {
     
     public static void main(String[] args) throws Exception {
+        LookAndFeel l = UIManager.getLookAndFeel();
+        try{
+            UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName() );
+        }catch(Exception ign){
+            UIManager.setLookAndFeel( l );
+        }
+        
         Map map = new HashMap();
-        map.put("default.host", "192.168.56.102:8080");
+        map.put("default.host", "10.0.0.108:8080");
         map.put("app.context", "bohol");
         OsirisTestPlatform.runTest(map);
     }
