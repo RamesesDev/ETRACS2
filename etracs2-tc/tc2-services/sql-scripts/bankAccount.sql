334017e27dc1863abf00cf8c3d8abda9c26d9d5c
Windhel Palen <rameses@rameses-desktop.(none)>
2011-05-24 13:06:43 +0800

    updated schema 	- bankaccount.xml updated page 	- BacnkAcctPage updated workunit 	- lupbank.xml updated sql-scripts 	- bankAccount.sql

--- etracs2-tc/tc2-services/sql-scripts/bankAccount.sql
+++ etracs2-tc/tc2-services/sql-scripts/bankAccount.sql
@@ -5,11 +5,13 @@ bankaccount CREATE TABLE 'bankaccount' (
    'docstate' varchar(50) NOT NULL,
    'accttype' varchar(50) NOT NULL,
    'acctno' varchar(50) NOT NULL,
+   'bankacctname' varchar(100) NOT NULL,
    'fund' varchar(50) NOT NULL,
    'bankid' varchar(50) NOT NULL,
    'bankcode' varchar(50) NOT NULL,
    'bankname' varchar(50) NOT NULL,
    'branchname' varchar(50) NOT NULL,
+   'currency' varchar(50) NOT NULL,
    'fundid' varchar(50) NOT NULL,
    PRIMARY KEY ('objid')
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1
