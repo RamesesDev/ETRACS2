b66d02e49953b8716d791b66cbdb94e7788dacec
rameses <rameses@rameses-dev07.(none)>
2011-06-09 15:32:11 +0800

    added kind of building transaction

---
+++ etracs2-rpt/rpt2-services/services.jar/rpt-services.jar/META-INF/sql/kindofbuilding.sql
@@ -0,0 +1,18 @@
+[getList]
+SELECT * FROM kindofbuilding ORDER BY bldgcode
+
+[getListByCode]
+SELECT * FROM kindofbuilding WHERE bldgcode LIKE $P{bldgcode} ORDER BY bldgcode
+
+[getListByKind]
+SELECT * FROM kindofbuilding WHERE bldgkind LIKE $P{bldgkind} ORDER BY bldgcode
+
+[getById]
+SELECT * FROM kindofbuilding WHERE objid = $P{objid}
+
+[checkDuplicateKind]
+SELECT COUNT(*) AS count FROM kindofbuilding WHERE bldgkind = $P{bldgkind}
+
+[checkDuplicateCode]
+SELECT COUNT(*) AS count FROM kindofbuilding WHERE bldgcode = $P{bldgcode}
+
