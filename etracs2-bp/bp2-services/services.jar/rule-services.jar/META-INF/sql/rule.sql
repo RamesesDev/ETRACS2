d76743a3f605534a889de30584a8faab756c4a7f
Jessie S. Zamora <jzamss@gmail.com>
2011-06-21 13:50:25 +0800

    Initial commit of BP Assessment Rule and BP Billing Rule
    Signed-off-by: Jessie S. Zamora <jzamss@gmail.com>

--- etracs2-bp/bp2-services/services.jar/rule-services.jar/META-INF/sql/rule.sql
+++ etracs2-bp/bp2-services/services.jar/rule-services.jar/META-INF/sql/rule.sql
@@ -1,12 +1,14 @@
 [getList]
-SELECT objid, docstate, agendagroup, rulename, description, author, salience 
+SELECT objid, docstate, agendagroup, rulename, description, author, salience, opener  
 FROM rule 
+WHERE ruleset = $P{ruleset}
 ORDER BY agendagroup, rulename  
 
 [getListByName]
-SELECT objid, docstate, agendagroup, rulename, description, author, salience 
+SELECT objid, docstate, agendagroup, rulename, description, author, salience, opener  
 FROM rule 
-WHERE rulename LIKE $P{name} 
+WHERE ruleset = $P{ruleset} 
+  AND rulename LIKE $P{name} 
 ORDER BY agendagroup, rulename  
 
 [getRuleGroups]
