c81dc00c8885353cc7eeee70083f3d744a0d0c3e
unknown <MS@.(none)>
2011-06-30 16:29:54 -0700

    update bp2-services/srvices.jar    BPApplicationService    BPRuleService    bprule.sql
    add etracs2-common/etracs2-common services/services.jar
    
    update etracs2-bp/bp2/src/etracs2/bp/application module

--- etracs2-bp/bp2-services/services.jar/bprule-services.jar/META-INF/sql/bprule.sql
+++ etracs2-bp/bp2-services/services.jar/bprule-services.jar/META-INF/sql/bprule.sql
@@ -6,6 +6,7 @@ SELECT
 	objid, 
 	name AS varname, 
 	datatype AS vartype, 
-	caption AS varcaption 
+	caption AS varcaption , 
+	arrayvalues AS arrayvalues 
 FROM variable 
 WHERE objid = $P{objid} 
