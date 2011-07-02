ddffa7c22ee4aac60dc506ee3341ffd05cb9c608
Jessie S. Zamora <jzamss@gmail.com>
2011-07-02 16:14:26 +0800

    RPTLedger initial commit
    Signed-off-by: Jessie S. Zamora <jzamss@gmail.com>

--- etracs2-rpt/rpt2-services/services.jar/rpt-services.jar/META-INF/sql/rptledger.sql
+++ etracs2-rpt/rpt2-services/services.jar/rpt-services.jar/META-INF/sql/rptledger.sql
@@ -1,8 +1,40 @@
 [getList]
-SELECT * FROM faaslist WHERE objid = $P{objid}
+SELECT objid, docstate, tdno, fullpin, taxpayername FROM rptledger WHERE docstate LIKE $P{docstate} 
+
+[getListByTdno]
+SELECT objid, docstate, tdno, fullpin, taxpayername FROM rptledger 
+WHERE docstate LIKE $P{docstate} AND tdno LIKE $P{tdno} 
+
+[getListByPin]
+SELECT objid, docstate, tdno, fullpin, taxpayername FROM rptledger 
+WHERE docstate LIKE $P{docstate} AND fullpin LIKE $P{fullpin} 
+
+[getListByTaxpayer]
+SELECT objid, docstate, tdno, fullpin, taxpayername FROM rptledger 
+WHERE docstate LIKE $P{docstate} AND taxpayername LIKE  $P{taxpayername} 
+
+
+[getListByCadastral]
+SELECT objid, docstate, tdno, fullpin, taxpayername FROM rptledger 
+WHERE docstate LIKE $P{docstate} AND cadastrallotno = $P{cadastrallotno} 
+
+[getListByBlock]
+SELECT objid, docstate, tdno, fullpin, taxpayername FROM rptledger 
+WHERE docstate LIKE $P{docstate} AND blockno = $P{blockno} 
+
 
 [getLatestItem]
 SELECT * FROM rptledgeritem WHERE toyear = 0 AND parentid = $P{parentid} 
+
+[getLedgerItemList]
+SELECT * FROM rptledgeritem WHERE parentid = $P{parentid} ORDER BY fromyear DESC 
+
+[getSmallestFromYear]
+SELECT MIN(fromyear) AS minfromyear FROM rptledgeritem WHERE parentid = $P{parentid} AND docstate = 'APPROVED' 
+
+
+
+
 
 
 
