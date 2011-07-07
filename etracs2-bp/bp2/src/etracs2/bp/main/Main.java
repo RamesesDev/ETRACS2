d4943a7ed711aa46eb0219ecf844c27e823a846b
Alvin Magalona <asmagalona@gmail.com>
2011-07-07 17:01:15 +0800

    BPLedger

--- etracs2-bp/bp2/src/etracs2/bp/main/Main.java
+++ etracs2-bp/bp2/src/etracs2/bp/main/Main.java
@@ -1,6 +1,7 @@
 package etracs2.bp.main;
 
 import com.rameses.osiris2.client.OsirisTestPlatform;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 import javax.swing.UIManager;
@@ -19,7 +20,7 @@ public class Main {
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         Map params = new HashMap();
         params.put("default.host", "localhost:8080");
-        params.put("app.context", "bohol");
+        params.put("app.context", "maribojoc");
         OsirisTestPlatform.runTest( params );
     }
     
