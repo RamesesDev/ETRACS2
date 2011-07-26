265b23a0a93f89e30470ac8f5b6b8ecd9a891f2a
Jessie S. Zamora <jzamss@gmail.com>
2011-07-26 19:30:55 +0800

    RPTBillingRule: - Add rptbilling rule support for Previous and Prior accounts
    RPTLedger:
    - Add Preview and Print billing support
    
    IncomeAccount:
    - Add systype for RPT Previous and Prior accounts
    
    ReportParameterService
    - move to common
    
    DateService
    - add new utility methods

--- etracs2-rpt/rpt2-services/services.jar/rpt-services.jar/META-INF/sql/rptbill.sql
+++ etracs2-rpt/rpt2-services/services.jar/rpt-services.jar/META-INF/sql/rptbill.sql
@@ -1,6 +1,7 @@
 [getOpenLedgersByTaxpayerId]
 SELECT 
-	objid, taxpayerid, fullpin AS pin, tdno , rputype, assessedvalue, barangay, classcode, txntype, 
+	objid, taxpayerid, fullpin AS pin, tdno , rputype, assessedvalue, 
+    barangay, classcode, txntype, cadastrallotno, 
 	CASE WHEN lastqtrpaid = 4 THEN lastyearpaid +1 ELSE lastyearpaid END AS fromyear, 
 	CASE WHEN lastqtrpaid = 4 THEN 1 ELSE lastqtrpaid + 1 END AS fromqtr, 
     lastyearpaid, lastqtrpaid, 
@@ -12,6 +13,21 @@ WHERE taxpayerid = $P{taxpayerid} AND docstate = 'APPROVED' AND taxable = 1
  AND ( lastyearpaid < $P{currentyr} OR (lastyearpaid = $P{currentyr} AND lastqtrpaid < 4 ) ) 
  
 
+[getOpenLedgersById]
+SELECT 
+	objid, taxpayerid, fullpin AS pin, tdno , rputype, assessedvalue, 
+    barangay, classcode, txntype, cadastrallotno, 
+	CASE WHEN lastqtrpaid = 4 THEN lastyearpaid +1 ELSE lastyearpaid END AS fromyear, 
+	CASE WHEN lastqtrpaid = 4 THEN 1 ELSE lastqtrpaid + 1 END AS fromqtr, 
+    lastyearpaid, lastqtrpaid, 
+	0 AS toyear, 0 AS toqtr, partialbasic, partialsef, 
+    0.0 AS basic, 0.0 AS basicint, 0.0 AS basicdisc,
+    0.0 AS sef, 0.0 AS sefint, 0.0 AS sefdisc 
+FROM rptledger 
+WHERE objid = $P{objid} AND docstate = 'APPROVED' AND taxable = 1 
+ AND ( lastyearpaid < $P{currentyr} OR (lastyearpaid = $P{currentyr} AND lastqtrpaid < 4 ) ) 
+ 
+ 
 [getOpenLedgerItems] 
 SELECT 
 	objid, tdno, txntype, classid, classcode, actualuseid, actualusecode, backtax,  
