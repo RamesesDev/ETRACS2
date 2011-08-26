2025645a1f2f84114fe98ef2472cbf8f157736e7
Teodoro dela Cerna jr <delacerna_teodoro@yahoo.com>
2011-08-26 12:01:21 +0800

    systeminfo controller 	permission added RPTCertficaions JR 	justified, adjusted tables etc. AF51 	adjusted RPTReceiptService 	added taxpayername rptcertificaion.sql 	modified statement: 	[getImprovementTDNo] 	[getWImproveLand] 	[getNoImprovementsTDNo]

--- etracs2-rpt/rpt2-services/services.jar/rpt-services.jar/META-INF/sql/rptcertification.sql
+++ etracs2-rpt/rpt2-services/services.jar/rpt-services.jar/META-INF/sql/rptcertification.sql
@@ -26,15 +26,6 @@ FROM rptcertification
 WHERE objid = $P{objid} 
 ORDER BY docno 
 
-[fetchData]
-SELECT 
-	objid, tdno, taxpayername, ownername, titleno, cadastrallotno, barangay, fullpin, 
-	rputype, totalmv, totalav, totalareaha, totalareasqm, classcode 
-FROM faaslist 
-WHERE taxpayerid = $P{taxpayerid} 
-	AND docstate = 'CURRENT' 
-	ORDER BY fullpin 
-
 [getLandHolding]
 SELECT 
 	objid, tdno, taxpayername, ownername, titleno, cadastrallotno, barangay, fullpin, 
@@ -72,17 +63,29 @@ WHERE taxpayerid = $P{taxpayerid}
 SELECT 
 	objid, tdno, taxpayername, ownername, titleno, cadastrallotno, barangay, fullpin, 
 	rputype, totalmv, totalav, totalareaha, totalareasqm, classcode 
-FROM faaslist 
-WHERE tdno = $P{tdno} 
-	AND landfaasid IS NULL  
+FROM faaslist f  
+WHERE tdno = $P{tdno}  
+	AND docstate = 'CURRENT' 
+	AND rputype = 'land' 
+	AND NOT EXISTS ( 
+		SELECT landfaasid FROM faaslist  
+		WHERE landfaasid = f.objid AND docstate <> 'CANCELLED' 
+	) 
+	ORDER BY fullpin 
 	
 [getImprovementsTDNo]
 SELECT 
 	objid, tdno, taxpayername, ownername, titleno, cadastrallotno, barangay, fullpin, 
 	rputype, totalmv, totalav, totalareaha, totalareasqm, classcode 
-FROM faaslist 
-WHERE tdno = $P{tdno} 
-	AND landfaasid IS NOT NULL  
+FROM faaslist f  
+WHERE tdno = $P{tdno}  
+	AND docstate = 'CURRENT' 
+	AND rputype = 'land' 
+	AND EXISTS ( 
+		SELECT landfaasid FROM faaslist  
+		WHERE landfaasid = f.objid AND docstate <> 'CANCELLED' 
+	) 
+	ORDER BY fullpin 
 
 [getWImproveLand]
 SELECT 
@@ -91,7 +94,12 @@ SELECT
 FROM faaslist f 
 WHERE taxpayerid = $P{taxpayerid} 
 	AND docstate = 'CURRENT' 
-	AND landfaasid IS NOT NULL 
+	AND rputype = 'land' 
+	AND EXISTS ( 
+		SELECT landfaasid FROM faaslist  
+		WHERE landfaasid = f.objid AND docstate <> 'CANCELLED' 
+	) 
+	ORDER BY fullpin 
 	
 [searchByTaxpayerid]
 select taxpayerid from faaslist where taxpayerid = $P{taxpayerid}  
