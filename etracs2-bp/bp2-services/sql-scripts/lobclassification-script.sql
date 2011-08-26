91d6ba368d185504efa58d5948e1a3d45aa901db
Jessie S. Zamora <jzamss@gmail.com>
2011-08-26 11:46:26 +0800

    BusinessPermit Report: modify standard etracs report
    Signed-off-by: Jessie S. Zamora <jzamss@gmail.com>

--- etracs2-bp/bp2-services/sql-scripts/lobclassification-script.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE lobclassification (                 
-	 objid varchar(50) NOT NULL,                    
-	 schemaname varchar(25) NOT NULL,               
-	 schemaversion varchar(5) NOT NULL,             
-	 name varchar(100) NOT NULL,                    
-	 remarks varchar(100) default NULL,             
-	 PRIMARY KEY  (objid),                          
-	 UNIQUE KEY ux_lobclassification_name (name)  
-   ) ENGINE=InnoDB DEFAULT CHARSET=latin1   
