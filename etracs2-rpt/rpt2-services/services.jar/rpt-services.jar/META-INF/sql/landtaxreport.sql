97dc466885e0eb0934ce29217477dc7e426b5281
Jessie S. Zamora <jzamss@gmail.com>
2011-09-17 16:45:20 +0800

    RPTReceiptService: - fix invalid ledgerid and ledgeritemid interchange posting value error - add rptledgeritemid column to rptpayment
    LiquidationService: fix txntimestamp value computation
    
    AbstractRPTCollection:
    - initial commit
    - add LandTaxReportService
    
    Signed-off-by: Jessie S. Zamora <jzamss@gmail.com>

---
+++ etracs2-rpt/rpt2-services/services.jar/rpt-services.jar/META-INF/sql/landtaxreport.sql
@@ -0,0 +1,108 @@
+[getCollectorList]
+SELECT objid AS collectorid, name AS collectorname, jobtitle AS collectortitle  from etracsuser WHERE iscollector = 1 ORDER BY name 
+
+
+[getAbstractCollectionBASIC] 
+SELECT  
+	rp.period AS payperiod, 
+	'BASIC' AS type, 
+	r.txndate AS ordate, 
+	rl.taxpayername, 
+	rl.tdno, 
+	r.serialno AS orno, 
+	rl.barangay, 
+	rl.classcode AS classification, 
+	IFNULL((SELECT SUM( basic ) FROM rptpaymentdetail WHERE receiptid = r.objid AND rptledgerid = rl.objid AND revtype ='current' ), 0.0) AS currentyear, 
+	IFNULL((SELECT SUM( basic ) FROM rptpaymentdetail WHERE receiptid = r.objid AND rptledgerid = rl.objid AND revtype ='previous' ), 0.0) AS previousyear, 
+	IFNULL((SELECT SUM( basicdisc ) FROM rptpaymentdetail WHERE receiptid = r.objid AND rptledgerid = rl.objid ), 0.0) AS discount, 
+	IFNULL((SELECT SUM( basicint ) FROM rptpaymentdetail WHERE receiptid = r.objid AND rptledgerid = rl.objid AND revtype ='current' ), 0.0) AS penaltycurrent, 
+	IFNULL((SELECT SUM( basicint ) FROM rptpaymentdetail WHERE receiptid = r.objid AND rptledgerid = rl.objid AND revtype ='previous' ), 0.0) AS penaltyprevious 
+FROM liquidationlist lq 
+	INNER JOIN remittancelist rem ON lq.objid = rem.liquidationid  
+	INNER JOIN receiptlist r ON rem.objid = r.remittanceid  
+	INNER JOIN rptpayment rp ON rp.receiptid = r.objid  
+	INNER JOIN rptledger rl ON rp.rptledgerid = rl.objid  
+WHERE lq.txntimestamp LIKE $P{txntimestamp} 
+  AND r.doctype = 'RPT'  
+  AND r.voided = 0  
+  AND r.collectorid LIKE $P{collectorid} 
+ORDER BY r.serialno, rl.tdno    
+
+[getAbstractCollectionSEF]
+SELECT 
+	rp.period AS payperiod, 
+	'SEF' AS type, 
+	r.txndate AS ordate, 
+	rl.taxpayername, 
+	rl.tdno, 
+	r.serialno AS orno, 
+	rl.barangay, 
+	rl.classcode AS classification, 
+	IFNULL((SELECT SUM( sef ) FROM rptpaymentdetail WHERE receiptid = r.objid AND rptledgerid = rl.objid AND revtype ='current' ),0.0) AS currentyear, 
+	IFNULL((SELECT SUM( basic ) FROM rptpaymentdetail WHERE receiptid = r.objid AND rptledgerid = rl.objid AND revtype ='previous' ),0.0) AS previousyear, 
+	IFNULL((SELECT SUM( sefdisc ) FROM rptpaymentdetail WHERE receiptid = r.objid AND rptledgerid = rl.objid ),0.0) AS discount, 
+	IFNULL((SELECT SUM( sefint ) FROM rptpaymentdetail WHERE receiptid = r.objid AND rptledgerid = rl.objid AND revtype ='current' ),0.0) AS penaltycurrent, 
+	IFNULL((SELECT SUM( sefint ) FROM rptpaymentdetail WHERE receiptid = r.objid AND rptledgerid = rl.objid AND revtype ='previous' ),0.0) AS penaltyprevious 
+FROM liquidationlist lq 
+	INNER JOIN remittancelist rem ON lq.objid = rem.liquidationid  
+	INNER JOIN receiptlist r ON rem.objid = r.remittanceid  
+	INNER JOIN rptpayment rp ON rp.receiptid = r.objid  
+	INNER JOIN rptledger rl ON rp.rptledgerid = rl.objid  
+WHERE lq.txntimestamp LIKE $P{txntimestamp}  
+  AND r.doctype = 'RPT'  
+  AND r.voided = 0  
+  AND r.collectorid LIKE $P{collectorid} 
+ORDER BY r.serialno, rl.tdno 
+
+
+[getAbstractCollectionManualBASIC]
+SELECT 
+	rp.period AS payperiod,
+	'BASIC' AS type,
+	r.txndate AS ordate,
+	rp.taxpayername,
+	rp.tdno,
+	r.serialno AS orno,
+	rp.barangay,
+	rp.classcode AS classification, 
+	rp.basic AS currentyear, 
+	rp.basicprev + rp.basicprior AS previousyear, 
+	rp.basicdisc AS discount, 
+	rp.basicint AS penaltycurrent, 
+	rp.basicprevint + rp.basicpriorint AS penaltyprevious 
+FROM liquidationlist lq
+	INNER JOIN remittancelist rem ON lq.objid = rem.liquidationid 
+	INNER JOIN receiptlist r ON rem.objid = r.remittanceid 
+	INNER JOIN rptpaymentmanual rp ON rp.receiptid = r.objid 
+WHERE lq.txntimestamp LIKE $P{txntimestamp}  
+  AND r.doctype = 'RPT' 
+  AND r.voided = 0 
+  AND r.collectorid LIKE $P{collectorid}
+
+
+
+[getAbstractCollectionManualSEF]
+SELECT 
+	rp.period AS payperiod,
+	'SEF' AS type,
+	r.txndate AS ordate,
+	rp.taxpayername,
+	rp.tdno,
+	r.serialno AS orno,
+	rp.barangay,
+	rp.classcode AS classification, 
+	rp.sef AS currentyear, 
+	rp.sefprev + rp.sefprior AS previousyear, 
+	rp.sefdisc AS discount, 
+	rp.sefint AS penaltycurrent, 
+	rp.sefprevint + rp.sefpriorint AS penaltyprevious 
+FROM liquidationlist lq
+	INNER JOIN remittancelist rem ON lq.objid = rem.liquidationid 
+	INNER JOIN receiptlist r ON rem.objid = r.remittanceid 
+	INNER JOIN rptpaymentmanual rp ON rp.receiptid = r.objid 
+WHERE lq.txntimestamp LIKE $P{txntimestamp}  
+  AND r.doctype = 'RPT' 
+  AND r.voided = 0 
+  AND r.collectorid LIKE $P{collectorid}
+
+
