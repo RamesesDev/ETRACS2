76ea014ead803753015d9dfe4948f421bb4d66e4
Teodoro dela Cerna jr <delacerna_teodoro@yahoo.com>
2011-09-21 08:27:06 +0800

    new Script FullyPaidScripts 	- to update fullypaid field to 1 BPCollectionInterceptor 	- add new method updateAppFullypid( param ) business.sql 	- added new statement updateAppFullypaid BPLedgerService 	- remove println

--- etracs2-bp/bp2-services/services.jar/bp-services.jar/META-INF/sql/business.sql
+++ etracs2-bp/bp2-services/services.jar/bp-services.jar/META-INF/sql/business.sql
@@ -36,8 +36,8 @@ INNER JOIN bpapplicationlisting l ON l.objid = b.objid
 WHERE b.businessid = $P{businessid} 
 ORDER BY b.txnno DESC 
 
-[updateAppFullypaid]
-UPDATE bpapplicationlisting 
-SET fullypaid = 1 
-WHERE businessid = $P{businessid} 
+[updateAppFullypaid]  
+UPDATE bpapplicationlisting   
+SET fullypaid = 1   
+WHERE businessid = $P{businessid}   
 
