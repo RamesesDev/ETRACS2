6b13e8cb8abc6cdcf0e4a61df41b56d32c3e90e0
Jessie S. Zamora <jzamss@gmail.com>
2011-10-02 20:20:32 +0800

    PostCapture: fix posting bug and add list management
    Signed-off-by: Jessie S. Zamora <jzamss@gmail.com>

--- etracs2-tc/tc2-services/services.jar/tc-services.jar/META-INF/sql/postcapture.sql
+++ etracs2-tc/tc2-services/services.jar/tc-services.jar/META-INF/sql/postcapture.sql
@@ -1,19 +1,29 @@
+[getList]
+SELECT 
+	objid, docstate, dtposted, collectorid, collectorname, collectortitle,  
+	postedbyname, amount  
+FROM postcapture  
+WHERE collectorname LIKE $P{collectorname}  
+ORDER BY dtposted DESC  
+
 [getCapturedReceiptsSummaries]
 SELECT 
 	afid, SUM( CASE WHEN voided = 0 THEN amount ELSE 0.0 END) AS amount, COUNT(*) AS icount  
 FROM receiptlist 
 WHERE collectorid = $P{collectorid} 
+  AND mode = 'CAPTURE'  
   AND docstate = 'CAPTURED' 
 GROUP BY afid 
 
-[getCapturedReceipts]
+[getCapturedReceipts] 
 SELECT 
 	objid, afid, serialno, opener, stubno, collectiontype, payorname, payoraddress, 
 	CASE WHEN voided = 0 THEN amount ELSE 0.0 END AS amount, 
 	voided, voidreason 
 FROM receiptlist 
 WHERE collectorid = $P{collectorid} 
-  AND docstate = 'CAPTURED'
+  AND mode = 'CAPTURE' 
+  AND docstate = 'CAPTURED'  
   AND afid = $P{afid} 
 ORDER BY stubno, serialno  
 
