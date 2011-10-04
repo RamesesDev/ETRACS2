5a7425556d1618eba8133eb889c9e633351ff480
Jessie S. Zamora <jzamss@gmail.com>
2011-10-04 19:18:45 +0800

    Lob : filter classification lookup
    Signed-off-by: Jessie S. Zamora <jzamss@gmail.com>

--- etracs2-bp/bp2-services/services.jar/bp-services.jar/META-INF/sql/lobclassification.sql
+++ etracs2-bp/bp2-services/services.jar/bp-services.jar/META-INF/sql/lobclassification.sql
@@ -1,8 +1,10 @@
 [getList]
-SELECT * FROM lobclassification  ORDER BY name 
+SELECT * FROM lobclassification  
+ORDER BY name 
 
 [getListByName]
-SELECT * FROM lobclassification WHERE name LIKE $P{name} ORDER BY name 
+SELECT * FROM lobclassification 
+WHERE name LIKE $P{name} ORDER BY name 
 
 [checkDuplicateName]
 SELECT * FROM lobclassification WHERE objid <> $P{objid} AND name = $P{name}
