b684b82f25d388225d1b4e111e0140a0305713b2
Jessie S. Zamora <jzamss@gmail.com>
2011-10-16 23:30:02 +0800

    RPTCollection Report : initial commit
    Signed-off-by: Jessie S. Zamora <jzamss@gmail.com>

--- etracs2-rpt/rpt2-services/services.jar/rpt-services.jar/META-INF/sql/landtaxreport.sql
+++ etracs2-rpt/rpt2-services/services.jar/rpt-services.jar/META-INF/sql/landtaxreport.sql
@@ -1,6 +1,16 @@
+[getClassificationList] 
+SELECT objid AS classid, propertydesc AS classname, special   
+FROM propertyclassification  ORDER BY orderno  
+
+[getExemptionList]
+SELECT objid AS classid, exemptdesc AS classname, 0 AS special 
+FROM exemptiontype ORDER BY orderno  
+
 [getBarangayList]
 SELECT objid AS barangayid, lguname AS barangay FROM lgu WHERE lgutype = 'BARANGAY' AND parentid = $P{parentid} ORDER BY lguname 
 
+[getBrgyCount]
+SELECT COUNT(*) AS brgycount FROM lgu WHERE lgutype = 'BARANGAY' 
 
 [getCollectorList]
 SELECT objid AS collectorid, name AS collectorname, jobtitle AS collectortitle  from etracsuser WHERE iscollector = 1 ORDER BY name 
@@ -151,5 +161,68 @@ GROUP BY rp.barangay
 ORDER BY rp.barangay   
 
 
+
+
+[getOnlineRPTC]
+SELECT 	
+	rl.classid, 
+	SUM( CASE WHEN rpd.revtype = 'current' THEN rpd.basic ELSE 0 END ) AS basiccurrent, 
+	SUM( CASE WHEN rpd.revtype = 'current' THEN rpd.basicdisc ELSE 0 END ) AS basicdisc, 
+	SUM( CASE WHEN rpd.revtype = 'previous' THEN rpd.basic  ELSE 0 END ) AS basicprev, 
+	SUM( CASE WHEN rpd.revtype = 'current' THEN rpd.basicint ELSE 0 END ) AS basiccurrentint, 
+	SUM( CASE WHEN rpd.revtype = 'previous' THEN rpd.basicint  ELSE 0 END ) AS basicprevint, 
+	SUM( rpd.basic + rpd.basicint ) AS basicgross, 
+	SUM( rpd.basic + rpd.basicint - rpd.basicdisc ) AS basicnet, 
+
+	SUM( CASE WHEN rpd.revtype = 'current' THEN rpd.sef ELSE 0 END ) AS sefcurrent, 
+	SUM( CASE WHEN rpd.revtype = 'current' THEN rpd.sefdisc ELSE 0 END ) AS sefdisc, 
+	SUM( CASE WHEN rpd.revtype = 'previous' THEN rpd.sef  ELSE 0 END ) AS sefprev, 
+	SUM( CASE WHEN rpd.revtype = 'current' THEN rpd.sefint ELSE 0 END ) AS sefcurrentint, 
+	SUM( CASE WHEN rpd.revtype = 'previous' THEN rpd.sefint  ELSE 0 END ) AS sefprevint, 
+	SUM( rpd.sef + rpd.sefint ) AS sefgross, 
+	SUM( rpd.sef + rpd.sefint - rpd.sefdisc ) AS sefnet,
+	SUM( rpd.basic + rpd.basicint + rpd.sef + rpd.sefint ) AS grandtotal, 
+	SUM( rpd.basic + rpd.basicint - rpd.basicdisc + rpd.sef + rpd.sefint - rpd.sefdisc ) AS netgrandtotal 
+FROM revenue r 
+	INNER JOIN receiptlist rct ON r.receiptid = rct.objid
+	INNER JOIN rptpayment rp ON r.receiptid = rp.receiptid
+	INNER JOIN rptledger rl ON rp.rptledgerid = rl.objid 
+	INNER JOIN rptpaymentdetail rpd ON r.receiptid = rpd.receiptid 
+WHERE r.voided = 0 
+  AND r.liquidationtimestamp LIKE $P{txntimestamp} 
+GROUP BY rl.classid 
+
+
+[getManualRPTC]
+SELECT 	
+	pc.objid AS classid ,
+	SUM( rpm.basic ) AS basiccurrent, 
+	SUM( rpm.basicdisc ) AS basicdisc, 
+	SUM( rpm.basicprev + rpm.basicprior ) AS basicprev, 
+	SUM( rpm.basicint ) AS basiccurrentint, 
+	SUM( rpm.basicprevint + rpm.basicpriorint ) AS basicprevint, 
+	SUM( rpm.basic + rpm.basicprev + rpm.basicprior + rpm.basicint + rpm.basicprevint + rpm.basicpriorint ) AS basicgross, 
+	SUM( rpm.basic + rpm.basicprev + rpm.basicprior + rpm.basicint + rpm.basicprevint + rpm.basicpriorint - rpm.basicdisc  ) AS basicnet,
+
+	SUM( rpm.sef ) AS sefcurrent, 
+	SUM( rpm.sefdisc ) AS sefdisc, 
+	SUM( rpm.sefprev + rpm.sefprior ) AS sefprev, 
+	SUM( rpm.sefint ) AS sefcurrentint, 
+	SUM( rpm.sefprevint + rpm.sefpriorint ) AS sefprevint, 
+	SUM( rpm.sef + rpm.sefprev + rpm.sefprior + rpm.sefint + rpm.sefprevint + rpm.sefpriorint ) AS sefgross,  
+	SUM( rpm.sef + rpm.sefprev + rpm.sefprior + rpm.sefint + rpm.sefprevint + rpm.sefpriorint - rpm.sefdisc  ) AS sefnet, 
+
+	SUM( rpm.basic + rpm.basicprev + rpm.basicprior + rpm.basicint + rpm.basicprevint + rpm.basicpriorint +
+	     rpm.sef + rpm.sefprev + rpm.sefprior + rpm.sefint + rpm.sefprevint + rpm.sefpriorint ) AS grandtotal,  
+
+	SUM( rpm.basic + rpm.basicprev + rpm.basicprior + rpm.basicint + rpm.basicprevint + rpm.basicpriorint + 
+	     rpm.sef + rpm.sefprev + rpm.sefprior + rpm.sefint + rpm.sefprevint + rpm.sefpriorint - rpm.basicdisc - rpm.sefdisc ) AS netgrandtotal  
+FROM revenue r  
+	INNER JOIN receiptlist rct ON r.receiptid = rct.objid 
+	INNER JOIN rptpaymentmanual rpm ON r.receiptid = rpm.receiptid 
+	LEFT JOIN propertyclassification pc ON rpm.classcode = pc.propertycode  
+WHERE r.voided = 0 
+  AND r.liquidationtimestamp LIKE $P{txntimestamp}  
+GROUP BY rpm.classcode  
 
 
