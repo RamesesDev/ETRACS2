d0b240c294e4d1099fd89e6f7cfced7cf7014cfa
Jessie S. Zamora <jzamss@gmail.com>
2011-10-25 19:40:37 +0800

    BatchCaptureService - add capturedbyid info to be able to filter records   encoded by a subcollector
    PostCaptureService
    - filter records that matches the collector or specific subcollector
    
    Signed-off-by: Jessie S. Zamora <jzamss@gmail.com>

--- etracs2-tc/tc2/src/etracs2/tc/collection/BatchCapturePage.java
+++ etracs2-tc/tc2/src/etracs2/tc/collection/BatchCapturePage.java
@@ -95,6 +95,7 @@ public class BatchCapturePage extends javax.swing.JPanel {
         xDataTable1.setBorder(xTitledBorder1);
         xDataTable1.setFont(new java.awt.Font("Tahoma", 0, 14));
         xDataTable1.setHandler("listHandler");
+        xDataTable1.setImmediate(true);
         xDataTable1.setName("selectedItem");
         xDataTable1.setShowRowHeader(true);
         add(xDataTable1, java.awt.BorderLayout.CENTER);
