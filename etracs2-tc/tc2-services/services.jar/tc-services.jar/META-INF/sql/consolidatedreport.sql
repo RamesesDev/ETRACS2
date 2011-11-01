958abbb2d1c6dc11d1effcdb4e64f7718250aaef
Teodoro dela Cerna jr <delacerna_teodoro@yahoo.com>
2011-11-01 23:36:46 +0800

    bplisting.xml 	+ added field -> fullypaid
    consolidatereport.sql
    	modified statement [updateCRAAFbyIRAFCol]
    	statements added :
    	+[getCraaf]
    	+[getAFInventory]
    	+[getAfICreditByParentId1]
    	+[getAfICreditByParentId]
    	+[getAFCtrl]
    	+[getCraafByAfinventorycreditid]
    	+[getAFInventoryQtyHandById]
    
    CRAAFInterceptor
    	added new method interceptBeforeIRAFApproved( evt )
    
    ConsolidatedReportService
    	added new method postToCraaf()

--- etracs2-tc/tc2-services/services.jar/tc-services.jar/META-INF/sql/consolidatedreport.sql
+++ etracs2-tc/tc2-services/services.jar/tc-services.jar/META-INF/sql/consolidatedreport.sql
@@ -46,19 +46,6 @@ AND craafmonth = $P{craafmonth}
 AND craafyear = $P{craafyear}   
 ORDER BY afid, beginfrom, receivedfrom 
 
-[getReportDataGeneralByOpenState]
-SELECT  i.docstate, c.collectorname, c.afid, c.beginqty, c.beginfrom, c.beginto,   
-	c.receivedqty, c.receivedfrom, c.receivedto,   
-	c.issuedqty, c.issuedfrom, c.issuedto,  
-	c.endingqty, c.endingfrom, c.endingto   
-FROM craaf  c, afinventory i 
-WHERE c.afinventoryid = i.objid 
-AND c.collectorid IS NULL     
-AND c.craafmonth < $P{currMonth}     
-AND c.craafyear <= $P{currYear}   
-AND i.docstate <> 'CLOSED'   
-ORDER BY c.afid, c.beginfrom, c.receivedfrom  
-
 [getReportDataCollector]
 SELECT a.collectorname, c.stubno, c.afid, c.beginqty, c.beginfrom, c.beginto,   
 	c.receivedqty, c.receivedfrom, c.receivedto,   
@@ -71,19 +58,6 @@ WHERE c.afinventorycreditid = a.afinventorycreditid
   AND craafyear = $P{craafyear}     
 ORDER BY c.collectorname, c.afid, c.beginfrom, c.receivedfrom  
 
-[getReportDataCollectorByOpenState]
-SELECT a.collectorname, c.stubno, c.afid, c.beginqty, c.beginfrom, c.beginto,   
-	c.receivedqty, c.receivedfrom, c.receivedto,   
-	c.issuedqty, c.issuedfrom, c.issuedto,    
-	c.endingqty, c.endingfrom, c.endingto    
-FROM craaf c , afcontrol a  
-WHERE c.afinventorycreditid = a.afinventorycreditid  
-  AND c.afinventorycreditid IS NOT NULL   
-  AND craafmonth < $P{currMonth}    
-  AND craafyear <= $P{currYear}  
-  AND a.docstate <> 'CLOSED' 
-ORDER BY c.collectorname, c.afid, c.beginfrom, c.receivedfrom  
-
 [getReportDataCanceledSeries]
 SELECT c.collectorname, a.stubno, c.afid, c.canceledqty AS beginqty,    
 	   c.canceledfrom AS beginfrom, c.canceledto AS beginto     
@@ -93,16 +67,6 @@ AND craafyear = $P{craafyear}
 AND craafmonth = $P{craafmonth}   
 AND canceledqty != 0  
 
-[getReportDataCanceledSeriesOpenState]
-SELECT c.collectorname, a.stubno, c.afid, c.canceledqty AS beginqty,    
-	   c.canceledfrom AS beginfrom, c.canceledto AS beginto     
-FROM craaf c, afcontrol a    
-WHERE c.afinventorycreditid = a.afinventorycreditid    
-AND craafyear < $P{currYear}  
-AND craafmonth <= $P{currMonth}   
-AND docstate <> 'CLOSED' 
-AND canceledqty != 0
-
 [updateCRAAFbyIRAFCol]
 UPDATE craaf SET 
 	issuedqty = issuedqty + $P{qtyreceived}, 
@@ -110,8 +74,11 @@ UPDATE craaf SET
 	issuedto = $P{issuedto}, 
 	endingqty = $P{endingqty}, 
 	endingfrom = $P{endingfrom}, 
-	endingto = $P{endingto}   
-WHERE afinventoryid = $P{afinventoryid} AND afid = $P{afid} 
+	endingto = $P{endingto}    
+WHERE afinventoryid = $P{afinventoryid}  
+ AND afid = $P{afid}  
+ AND craafyear = $P{year}  
+ AND craafmonth = $P{month} 
 
 [updateCRAAFbyRemittance] 
 UPDATE craaf SET 
@@ -121,4 +88,57 @@ UPDATE craaf SET
 	endingqty = $P{endingqty}, 
 	endingfrom = $P{endingfrom}, 
 	endingto = $P{endingto}  
+WHERE objid = $P{objid} 
+
+
+
+----- post to craaf statements -----
+
+[getCraaf]
+SELECT 
+ *  
+FROM craaf 
+WHERE craafyear = $P{year}  
+ AND craafmonth = $P{month}
+
+[getAFInventory]
+SELECT 
+ * 
+ FROM afinventory 
+ 
+[getAfICreditByParentId1]
+SELECT 
+ afic.objid, afctrl.docstate, 
+ afctrl.collectorid, afctrl.collectorname, 
+ afctrl.collectortitle, afctrl.beginqty, 
+ afctrl.beginseries, afctrl.endseries
+FROM afinventorycredit afic 
+INNER JOIN afcontrol afctrl ON afctrl.afinventorycreditid = afic.objid 
+WHERE afinventoryid = $P{afinventoryid} 
+ AND afctrl.docstate = 'APPROVED' 
+
+[getAfICreditByParentId]  
+SELECT  
+ * 
+FROM afinventorycredit   
+WHERE afinventoryid = $P{afinventoryid}  
+ AND docstate <> 'CLOSED'
+
+[getAFCtrl]
+SELECT 
+ * 
+FROM afcontrol 
+WHERE afinventorycreditid = $P{afinventorycreditid}  
+ and docstate = 'APPROVED'  
+ 
+[getCraafByAfinventorycreditid]
+SELECT afinventorycreditid FROM craaf 
+WHERE afinventorycreditid = $P{afinventorycreditid} 
+ AND craafyear = $P{year}  
+ AND craafmonth = $P{month} 
+ 
+[getAFInventoryQtyHandById]
+SELECT 
+ qtyonhand 
+ FROM afinventory  
 WHERE objid = $P{objid} 
