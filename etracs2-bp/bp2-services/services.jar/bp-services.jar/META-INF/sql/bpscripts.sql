35e61b10d596dddd8a7ac96b3611cab5106eb1e3
Teodoro dela Cerna jr <delacerna_teodoro@yahoo.com>
2012-01-06 09:50:45 +0800

    added new scripts method to paid all receivables in a particular application

--- etracs2-bp/bp2-services/services.jar/bp-services.jar/META-INF/sql/bpscripts.sql
+++ etracs2-bp/bp2-services/services.jar/bp-services.jar/META-INF/sql/bpscripts.sql
@@ -5,14 +5,22 @@ UPDATE bpapplicationlisting
 SET fullypaid = 1   
 WHERE businessid = $P{businessid} 
 
-
 [updateStateApps]
 UPDATE bpapplication   
 SET docstate = 'PERMIT_PENDING'   
 WHERE objid = $P{objid} 
 
-
 [updateStateAppListing]
 UPDATE bpapplicationlisting   
 SET docstate = 'PERMIT_PENDING'   
+WHERE objid = $P{objid} 
+
+[getreceivables]
+SELECT objid, receivables 
+FROM bpapplication 
+WHERE tradename = $P{tradename}
+
+[updatereceivables]
+UPDATE bpapplication 
+SET receivables = $P{receivables} 
 WHERE objid = $P{objid} 
