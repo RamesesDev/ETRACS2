c88c98166fb6c5a9415149678af8e036b6b01f91
Jessie S. Zamora <jzamss@gmail.com>
2012-01-17 11:39:53 +0800

    Liquidation and Deposit: Add support for liquidation by fund and multi-cashier
    Signed-off-by: Jessie S. Zamora <jzamss@gmail.com>

--- patches/patch-2.01.015/Patch-2.01.015 Update Script.sql
+++ patches/patch-2.01.015/Patch-2.01.015 Update Script.sql
@@ -26,3 +26,67 @@ UPDATE rptpaymentmanual r, lgu l   SET
 WHERE l.lgutype = 'CITY';
 
 
+/* ------------------------------------------------------------------
+*
+* MULTI-CASHIER SUPPORT 
+*
+------------------------------------------------------------------ */
+
+alter table liquidation add column rcds text;
+update liquidation set rcds = '[]';
+
+alter table liquidation add column opener varchar(50) not null;
+alter table liquidationlist add column opener varchar(50) not null;
+
+update liquidation set opener='single';
+update liquidationlist set opener='single';
+
+CREATE TABLE `liquidationrcd` (                                                                     
+  `objid` varchar(50) NOT NULL,        
+  `docstate` varchar(20) NOT NULL,        
+  `schemaname` varchar(25) NOT NULL,                                                                
+  `schemaversion` varchar(5) NOT NULL,                                                              
+  `liquidationid` varchar(50) NOT NULL,                                                             
+  `liquidationno` varchar(25) NOT NULL,                                                             
+  `liquidationdate` date NOT NULL,                                                                  
+  `lqofficerid` varchar(50) NOT NULL,                                                               
+  `lqofficername` varchar(100) NOT NULL,                                                            
+  `lqofficertitle` varchar(50) NOT NULL,                                                            
+  `fundid` varchar(50) NOT NULL,                                                                    
+  `fundname` varchar(100) NOT NULL,                                                                 
+  `cashierid` varchar(50) NOT NULL,                                                                 
+  `cashiername` varchar(100) NOT NULL,                                                              
+  `cashiertitle` varchar(50) NOT NULL,                                                              
+  `cash` decimal(14,2) NOT NULL,                                                                    
+  `noncash` decimal(14,2) NOT NULL,                                                                 
+  `amount` decimal(14,2) NOT NULL,                                                                  
+  `denominations` text NOT NULL,                                                                    
+  `noncashpayments` text NOT NULL,                                                                        
+  `depositid` varchar(50) NULL, 
+  `dtdeposited` date null, 
+  PRIMARY KEY  (`objid`),                                                                           
+  KEY `FK_liquidationrcd` (`liquidationid`),                                                        
+  KEY `FK_liquidationrcd_fund` (`fundid`),                                                          
+  KEY `lqofficerid` (`lqofficerid`),                                                                
+  KEY `ix_liquidationrcd_cashierid` (`cashierid`),                                                  
+  CONSTRAINT `FK_liquidationrcd` FOREIGN KEY (`liquidationid`) REFERENCES `liquidation` (`objid`),  
+  CONSTRAINT `FK_liquidationrcd_fund` FOREIGN KEY (`fundid`) REFERENCES `fund` (`objid`)            
+) ENGINE=InnoDB DEFAULT CHARSET=latin1   ;
+
+
+
+alter table paymentitem add column liquidationid varchar(50);
+alter table paymentitem add column liquidationrcdid varchar(50);
+
+create index ix_paymentitem_liquidationid on paymentitem( liquidationid );
+create index ix_paymentitem_liquidationrcdid on paymentitem( liquidationrcdid );
+
+alter table receiptitem add column liquidationrcdid varchar(50);
+create index ix_receiptitem_liquidationrcdid on receiptitem(liquidationrcdid );
+
+alter table revenue add column liquidationrcdid varchar(50);
+create index ix_revenue_liquidationrcdid on revenue(liquidationrcdid );
+
+
+
+
