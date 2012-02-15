6c2b944b7cb0ea1d6389561a8749f113c40ae8ad
Jessie S. Zamora <jzamss@gmail.com>
2012-02-15 12:00:00 +0800

    RemittanceRCD: Add collection summary (group by fund) items.
    Signed-off-by: Jessie S. Zamora <jzamss@gmail.com>

--- etracs2-tc/tc2-services/services.jar/tc-services.jar/META-INF/sql/remittance.sql
+++ etracs2-tc/tc2-services/services.jar/tc-services.jar/META-INF/sql/remittance.sql
@@ -37,6 +37,24 @@ WHERE isliquidating = 1
 ORDER BY name 
 
 
+[getCollectionSummaryByAF]
+SELECT 
+	CASE 
+	WHEN af.objid = '51' AND af.aftype = 'serial' AND ia.groupid IS NULL THEN CONCAT( 'AF#', rl.afid, ': ', ri.fundname ) 
+	WHEN af.objid = '51' AND af.aftype = 'serial' AND ia.groupid IS NOT NULL THEN CONCAT( 'AF#', rl.afid, ': ', ia.groupid ) 
+	WHEN af.aftype = 'nonserial' AND ia.groupid IS NOT NULL THEN CONCAT( rl.afid, ': ', ia.groupid ) 
+	ELSE CONCAT( 'AF#',rl.afid, ': ', af.description,' - ', ri.fundname ) 
+	END AS particulars, 
+	SUM( ri.amount ) AS  amount   
+FROM receiptitem ri   
+INNER JOIN incomeaccount ia ON ri.acctid = ia.objid  
+INNER JOIN receiptlist rl on rl.objid = ri.receiptid    
+INNER JOIN remittancelist rml on rml.objid = rl.remittanceid     
+INNER JOIN af af ON rl.afid = af.objid 
+WHERE rml.objid = $P{remittanceid} 
+  AND rl.voided = 0   
+GROUP BY rl.afid, CASE WHEN af.aftype = 'nonserial' THEN ri.fundname ELSE CASE WHEN ia.groupid IS NULL THEN ri.fundname ELSE ia.groupid END  END  
+ORDER BY rl.afid, ri.fundname, ia.groupid 
 
 
 [getReceiptsByRemittance]
