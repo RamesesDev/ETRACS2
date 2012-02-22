ab310be5d676708ecade4d4db5b7e3ea48d6319b
Teodoro dela Cerna jr <delacerna_teodoro@yahoo.com>
2012-02-22 23:24:04 +0800

    entity.sql 	- contactno field omitted EntityService 	omitted 	- entity.contactno = ( entity.info.contactno ) ? entity.info.contactno : null entity.xml 	omitted 	- <field name="contactno" />
    BusinessPermit_jasper modified font

--- etracs2-common/etracs2-common services/services.jar/entity-services.jar/META-INF/sql/entity.sql
+++ etracs2-common/etracs2-common services/services.jar/entity-services.jar/META-INF/sql/entity.sql
@@ -1,15 +1,15 @@
 [getList] 
-SELECT objid, schemaname, schemaversion, entityno, entitytype, entityname, entityaddress, contactno  
+SELECT objid, schemaname, schemaversion, entityno, entitytype, entityname, entityaddress 
 FROM entity 
 ORDER BY entityname 
 
 [getListByNo] 
-SELECT objid, schemaname, schemaversion, entityno, entitytype, entityname, entityaddress, contactno  
+SELECT objid, schemaname, schemaversion, entityno, entitytype, entityname, entityaddress  
 FROM entity 
 WHERE entityno = $P{entityno} 
 
 [getListByName] 
-SELECT objid, schemaname, schemaversion, entityno, entitytype, entityname, entityaddress, contactno   
+SELECT objid, schemaname, schemaversion, entityno, entitytype, entityname, entityaddress 
 FROM entity 
 WHERE entityname LIKE $P{entityname} 
 ORDER BY entityname 
