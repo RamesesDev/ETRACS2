a466d24888a2017fce78e0f23df54cd10b169fbf
Jessie S. Zamora <jzamss@gmail.com>
2012-03-27 14:21:05 +0800

    messaging: change field message to status - process messages by concern

--- etracs2-common/etracs2-common services/services.jar/messaging-services.jar/META-INF/sql/message.sql
+++ etracs2-common/etracs2-common services/services.jar/messaging-services.jar/META-INF/sql/message.sql
@@ -5,7 +5,7 @@ SELECT objid FROM sys_outbox
 SELECT objid FROM sys_outbox WHERE tolguindex = $P{tolguindex} 
 
 [getInboxMessageId]
-SELECT objid FROM sys_inbox 
+SELECT objid FROM sys_inbox WHERE tolguindex = $P{tolguindex}  
 
 [getOutboxMessageByRefId]
 SELECT objid FROM sys_outbox WHERE refid = $P{refid} 
