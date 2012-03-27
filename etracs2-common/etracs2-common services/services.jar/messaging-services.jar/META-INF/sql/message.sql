aa08901c253dd72766860a3989134f5d391b4321
Jessie S. Zamora <jzamss@gmail.com>
2012-03-27 21:59:30 +0800

    Messaging: dispatch messages for other destinations to outbox
    Signed-off-by: Jessie S. Zamora <jzamss@gmail.com>

--- etracs2-common/etracs2-common services/services.jar/messaging-services.jar/META-INF/sql/message.sql
+++ etracs2-common/etracs2-common services/services.jar/messaging-services.jar/META-INF/sql/message.sql
@@ -9,3 +9,6 @@ SELECT objid FROM sys_inbox WHERE tolguindex = $P{tolguindex}
 
 [getOutboxMessageByRefId]
 SELECT objid FROM sys_outbox WHERE refid = $P{refid} 
+
+[getOtherDestinationMessages]
+SELECT objid FROM sys_inbox WHERE tolguindex <> $P{tolguindex}  
