7c882eb4580ddc03bac23c051a560bbda2227899
Jessie S. Zamora <jzamss@gmail.com>
2012-04-02 09:26:20 +0800

    provrpt-services.jar: cleanup redundant codes
    Signed-off-by: Jessie S. Zamora <jzamss@gmail.com>

--- etracs2-province/services.jar/prov-rpt-services.jar/META-INF/sql/rpu.sql
+++
@@ -1,5 +0,0 @@
-[getRPTParameter]
-SELECT 
-    objid AS paramid, paramname, paramcaption, paramdesc AS description, 
-    paramtype AS datatype,  parammin AS minvalue, parammax AS maxvalue 
-FROM rptparameters WHERE paramname = $P{name} 
